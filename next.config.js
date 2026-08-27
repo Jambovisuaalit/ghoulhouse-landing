da955cac3a000c95cfa8232d6ec87fd17b533949
Jamppa <132147167+Jambovisuaalit@users.noreply.github.com>
2026-08-27 13:58:25 +0300

    P0: harden next.config.js

--- next.config.js
+++ next.config.js
@@ -2,18 +2,21 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    formats: ['image/webp', 'image/avif'],
+    formats: ['image/avif', 'image/webp'],
     deviceSizes: [320, 375, 390, 430, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
-  headers: async () => {
+  async headers() {
     return [
       {
         source: '/:path*',
         headers: [
+          { key: 'X-DNS-Prefetch-Control', value: 'on' },
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+          { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
           {
-            key: 'X-DNS-Prefetch-Control',
-            value: 'on',
+            key: 'Permissions-Policy',
+            value: 'camera=(), microphone=(), geolocation=()',
           },
         ],
       },
