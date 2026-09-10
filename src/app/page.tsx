2432c12d1366ccd99502309fdb0780c83c85f4e1
Jamppa <132147167+Jambovisuaalit@users.noreply.github.com>
2026-09-10 23:41:46 +0300

    refactor: keep company data visible in footer only

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import EditorialLanding from '@/components/EditorialLanding';
 import FunnelAnalytics from '@/components/analytics/FunnelAnalytics';
-import { siteConfig } from '@/config/site';
 
 export default function Home() {
   return (
@@ -8,9 +7,6 @@ export default function Home() {
       <a className="skip-link" href="#main-content">
         Siirry pääsisältöön
       </a>
-      <span className="sr-only">
-        Kotipaikka {siteConfig.company.domicile}. {siteConfig.company.registrationStatus}
-      </span>
       <FunnelAnalytics />
       <EditorialLanding />
     </>
