0be7caa98fad3b2a31d1656c24a7d4abcebdab84
Jamppa <132147167+Jambovisuaalit@users.noreply.github.com>
2026-09-12 05:36:59 +0300

    fix: satisfy Next Script lint for GA4 bootstrap

--- src/components/analytics/GoogleAnalytics.tsx
+++ src/components/analytics/GoogleAnalytics.tsx
@@ -8,9 +8,9 @@ export default function GoogleAnalytics() {
       <Script
         id="google-analytics-src"
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
-        strategy="beforeInteractive"
+        strategy="afterInteractive"
       />
-      <Script id="google-analytics-init" strategy="beforeInteractive">
+      <Script id="google-analytics-init" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
